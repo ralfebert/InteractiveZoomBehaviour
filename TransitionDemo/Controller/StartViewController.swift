70262c3fca7446f38acb6b572caaaf73fbd34b86
Ralf Ebert <info@ralfebert.de>
2018-09-10 20:18:28 +0200

    extracted behaviour class

--- TransitionDemo/Controller/StartViewController.swift
+++ TransitionDemo/Controller/StartViewController.swift
@@ -1,15 +1,37 @@
 import UIKit
 
+class ZoomBehaviour : NSObject {
+
+    var originView : UIView
+    weak var originViewController : UIViewController?
+    var controllerToPresent : () -> (UIViewController)
+
+    init(originView : UIView, originViewController : UIViewController, controllerToPresent : @escaping () -> (UIViewController)) {
+        self.originView = originView
+        self.originViewController = originViewController
+        self.controllerToPresent = controllerToPresent
+        super.init()
+
+        self.originView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:))))
+    }
+
+    @IBAction func handleTap(_: UITapGestureRecognizer) {
+        let controller = controllerToPresent()
+        originViewController?.present(controller, animated: true, completion: nil)
+    }
+
+}
+
 class StartViewController: UIViewController {
 
     @IBOutlet var orangeView: UIView!
 
+    var zoomBehaviour : ZoomBehaviour!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.zoomBehaviour = ZoomBehaviour(originView: self.orangeView, originViewController: self) { ContentViewController() }
     }
 
-    @IBAction func handleViewTap(_: UITapGestureRecognizer) {
-        let controller = ContentViewController()
-        self.present(controller, animated: true, completion: nil)
-    }
 }
