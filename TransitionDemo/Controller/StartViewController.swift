0e1d3c61dac6733beaa89709de5557143efdffc0
Ralf Ebert <info@ralfebert.de>
2018-09-10 20:35:50 +0200

    transition

--- TransitionDemo/Controller/StartViewController.swift
+++ TransitionDemo/Controller/StartViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ZoomBehaviour : NSObject {
+class ZoomBehaviour : NSObject, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
 
     var originView : UIView
     weak var originViewController : UIViewController?
@@ -17,7 +17,87 @@ class ZoomBehaviour : NSObject {
 
     @IBAction func handleTap(_: UITapGestureRecognizer) {
         let controller = controllerToPresent()
+        controller.transitioningDelegate = self
         originViewController?.present(controller, animated: true, completion: nil)
+    }
+
+    // MARK: - UIViewControllerTransitioningDelegate
+
+    enum State {
+        case present
+        case dismiss
+    }
+
+    var state = State.present
+
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.animator = nil
+        self.state = .present
+        return self
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.animator = nil
+        self.state = .dismiss
+        return self
+    }
+
+    // MARK: - UIViewControllerAnimatedTransitioning
+
+    private var animator : UIViewPropertyAnimator?
+
+    func interruptibleAnimator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
+
+        if let animator = self.animator { return animator }
+
+        let animator = UIViewPropertyAnimator(duration: 0.3, curve: .linear)
+
+        let containerView = transitionContext.containerView
+
+        let fromViewController = transitionContext.viewController(forKey: .from)!
+        let fromView = transitionContext.view(forKey: .from)!
+        let fromViewFrame = transitionContext.finalFrame(for: fromViewController)
+
+        let toViewController = transitionContext.viewController(forKey: .to)!
+        let toView = transitionContext.view(forKey: .to)!
+        let toViewFrame = transitionContext.finalFrame(for: toViewController)
+
+        let originFrame = self.originView.convert(self.originView.bounds, to: containerView)
+
+        switch state {
+        case .present:
+            containerView.addSubview(toView)
+            toView.clipsToBounds = true
+            toView.frame = originFrame
+
+            animator.addAnimations {
+                toView.frame = toViewFrame
+            }
+
+        case .dismiss:
+            containerView.insertSubview(toView, belowSubview: fromView)
+
+            animator.addAnimations {
+                fromView.frame = originFrame
+            }
+
+        }
+
+        animator.addCompletion { (_) in
+            self.animator = nil
+            transitionContext.completeTransition(true)
+        }
+
+        self.animator = animator
+        return animator
+    }
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        self.interruptibleAnimator(using: transitionContext).startAnimation()
     }
 
 }
