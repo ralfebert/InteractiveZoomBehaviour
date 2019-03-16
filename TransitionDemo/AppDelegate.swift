8785f8119bba03df00ce7d07d8d688dcfeeaa173
Ralf Ebert <info@ralfebert.de>
2019-03-16 14:00:18 +0100

    swift 4.2

--- TransitionDemo/AppDelegate.swift
+++ TransitionDemo/AppDelegate.swift
@@ -5,7 +5,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = StartViewController()
